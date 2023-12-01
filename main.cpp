6d72e16670d7a67c8cf16750bcac0725c01496c2
elijahsandler <sandler.e@northeastern.edu>
2023-12-01 18:10:40 -0500

    popen() failed

--- main.cpp
+++ main.cpp
@@ -8,13 +8,10 @@ int main() {
     std::vector<double> x = linspace(0, 2 * pi);
     std::vector<double> y = transform(x, [](auto x) { return sin(x); });
 
+
     cout << "about to plot" << endl;
-    plot(x, y, "-o");
+    plot(x, y, "-");
     cout << "plot called" << endl;
-    hold(on);
-    plot(x, transform(y, [](auto y) { return -y; }), "--xr");
-    plot(x, transform(x, [](auto x) { return x / pi - 1.; }), "-:gs");
-    plot({1.0, 0.7, 0.4, 0.0, -0.4, -0.7, -1}, "k");
 
     show();
     return 0;
