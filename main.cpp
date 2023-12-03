4dafca11feb1a5fabb80fe2a2ec2b5384c244438
elijahsandler <sandler.e@northeastern.edu>
2023-12-03 17:10:50 -0500

    matplot++ works

--- main.cpp
+++ main.cpp
@@ -1,41 +1,43 @@
-//#include <cmath>
-//#include <matplot/matplot.h>
-//#include <iostream>
-//
-//
-//using namespace matplot; using namespace std;
-//
-//int main() {
-//
-//    std::vector<double> x = linspace(0, 2 * pi);
-//    std::vector<double> y = transform(x, [](auto x) { return sin(x); });
-//
-//
-//    cout << "about to plot" << endl;
-//    plot(x, y, "-");
-//    cout << "plot called" << endl;
-//
-//    show();
-//    return 0;
-//}
+#include <cmath>
+#include <matplot/matplot.h>
+#include <iostream>
+
+
+using namespace matplot; using namespace std;
+
+int main() {
+
+    std::vector<double> x = linspace(0, 2 * pi);
+    std::vector<double> y = transform(x, [](auto x) { return sin(x); });
+
+
+    cout << "about to plot" << endl;
+    plot(x, y);
+    cout << "plot called" << endl;
+
+    show();
+    return 0;
+}
 
 #include <iostream>
 #include <fstream>
 #include <commata/parse_csv.hpp>
 #include <commata/stored_table.hpp>
 
-using commata::make_stored_table_builder;
-using commata::parse_csv;
-using commata::stored_table;
-using namespace std;
-
-int main() {
-
-    stored_table colorTable;
-
-    parse_csv(ifstream("../color_srgb.csv"), make_stored_table_builder(colorTable));
-
-    cout << colorTable.size()-1 << " colors loaded" << endl;
-
-    return 0;
-}
+//using commata::make_stored_table_builder;
+//using commata::parse_csv;
+//using commata::stored_table;
+//using namespace std;
+//
+//int main() {
+//
+//    stored_table colorTable;
+//
+//    parse_csv(ifstream("../color_srgb.csv"), make_stored_table_builder(colorTable));
+//
+//    cout << colorTable.size()-1 << " colors loaded" << endl;
+//
+//
+//
+//    return 0;
+//}
