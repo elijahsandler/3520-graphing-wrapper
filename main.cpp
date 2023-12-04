037f2e85078e55d19a587828f7d2b33ea7e89415
elijahsandler <sandler.e@northeastern.edu>
2023-12-04 16:17:34 -0500

    god is dead

--- main.cpp
+++ main.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
-#include <fdeep/fdeep.hpp>
+#include <matplot/matplot.h>
+using namespace matplot;
 
-int main() {
-    const auto model = fdeep::load_model("output_json.json");
-//    const auto result = model.predict(
-//            {fdeep::tensor(fdeep::tensor_shape(static_cast<std::size_t>(3)),
-//                           std::vector<float>{.21, .32, .83})});
-//    std::cout << fdeep::show_tensors(result) << std::endl;
+int main(int argc, char *argv[]) {
+
+    std::vector<double> lon = iota(-170, 10, 170);
+    std::vector<double> lat =
+            transform(lon, [](double x) { return 50. * cosd(3 * x); });
+    std::vector<double> A = transform(
+            lon, [](double lon) { return (101. + 100 * (sind(2 * lon))) / 7; });
+    std::vector<double> C =
+            transform(lon, [](double lon) { return cosd(4 * lon); });
+
+    geoscatter(lat, lon, A, C)
+            ->marker_style(line_spec::marker_style::upward_pointing_triangle);
+
+    show();
+    return 0;
+
     return 0;
 }
