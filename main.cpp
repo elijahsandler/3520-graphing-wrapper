25860d04a5d67bbcb15ffe2ca21fef5efe9ec12d
elijahsandler <sandler.e@northeastern.edu>
2023-12-04 11:12:55 -0500

    fd cloned

--- main.cpp
+++ main.cpp
@@ -1,23 +1,14 @@
 #include <cmath>
 #include <matplot/matplot.h>
 #include <iostream>
-
-
+#include <cstdlib>
 using namespace matplot; using namespace std;
 
 int main() {
+    const char* runPyCmd = "python ../frugally-deep/keras_export/convert_model.py regression_nn.keras fdeep_model.json";
+    int result = system(runPyCmd);
 
-    std::vector<double> x = linspace(0, 2 * pi);
-    std::vector<double> y = transform(x, [](auto x) { return sin(x); });
-
-
-    cout << "about to plot" << endl;
-
-    plot(x, y);
-    cout << "plot called" << endl;
-
-    show();
-    return 0;
+    return result;
 }
 
 #include <iostream>
