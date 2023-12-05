fa4ecf3aadd76c5eebac3389467518d391f3ec27
elijahsandler <sandler.e@northeastern.edu>
2023-12-05 17:12:47 -0500

    ui added

--- main.cpp
+++ main.cpp
@@ -6,10 +6,29 @@
 
 using namespace matplot; using namespace std;
 
+void ui(Geographer geo) {
+    string input;
+    string prompt = "Enter 'scatter', 'geobubble', 'histogram' to create plot. "
+                    "\nEnter 'done' to exit. \n";
+
+    do {
+        cout << prompt;
+        cin >> input;
+        cout << endl;
+
+        if(input == "scatter") {geo.scatterplot();}
+        else if(input == "geobubble") {geo.geobubble();}
+        else if(input == "histogram") {geo.histogram();}
+        else if(input != "done") {cout << "Invalid input." << endl;}
+
+    } while(input != "done");
+    cout << "Thank you.";
+}
+
 int main(int argc, char *argv[]) {
 
     Geographer geo("../worldcities_temp.csv");
-    geo.scatterplot();
+    ui(geo);
 
     return 0;
 }
